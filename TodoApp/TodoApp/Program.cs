2dc596475df3e9a276d006c15a69df7fb442de1c
Csilla Varga <vargacsillamagda@gmail.com>
2017-09-28 16:18:06 +0200

    feat(RemoveNthTask) - removes task at the desired index

--- TodoApp/TodoApp/Program.cs
+++ TodoApp/TodoApp/Program.cs
@@ -24,12 +24,38 @@ namespace TodoApp
             {
                 AddNewTask(args);
             }
+            else if (args.Contains("-r"))
+            {
+                RemoveNthTask(args[1]);
+            }
             else
             {
-                Console.WriteLine("Unsupported argument. Try the following:");
+                Console.WriteLine("\nUnsupported argument. Try the following:");
                 PrintCommands();
             }
             Console.ReadLine();
+        }
+
+        private static void RemoveNthTask(string nth)                         // should it have an int az input parameter? it would make more sense - DEMO material?
+        {
+            string path = @"../../../../tasks.txt";
+            string[] content = File.ReadAllLines(path);
+
+            if (content.Length >= 2)
+            {
+                int index;                                                              
+                int.TryParse(nth, out index);                                  // simple Parse might be enough -- or use the bool it gives
+                List<string> newTaskList = content.ToList();
+                newTaskList.RemoveAt(index - 1);                                // if var is called index, it should be index, not index+1...
+
+                using (StreamWriter writer = new StreamWriter(path))
+                {
+                    foreach (var newTask in newTaskList)
+                    {
+                        writer.WriteLine(newTask);
+                    }
+                }
+            }
         }
 
         private static void AddNewTask(string[] newTask)
