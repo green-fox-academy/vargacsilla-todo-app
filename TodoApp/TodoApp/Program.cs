dabd87137d64a2d07647e49cc0f566d0082cb8ac
Csilla Varga <vargacsillamagda@gmail.com>
2017-09-28 11:12:39 +0200

    feat(PrintUsage) - long solution, probably needs refactoring

---
+++ TodoApp/TodoApp/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TodoApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PrintUsage();
+            Console.ReadLine();
+        }
+
+        private static void PrintUsage()
+        {
+            Console.WriteLine("Command Line Todo application \n=============================\n\nCommand line arguments:");
+            Console.WriteLine("-l   Lists all the tasks");
+            Console.WriteLine("-a   Adds a new task");
+            Console.WriteLine("-r   Removes a task");
+            Console.WriteLine("-c   Completes a task");
+        }
+    }
+}
